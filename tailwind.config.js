b781081b2b750660b0da2c8f9d07133d29eb026b
Aleks Lovestech <alekslovestech@gmail.com>
2025-06-25 00:13:40 +0200

    tweak

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,23 +6,23 @@ module.exports = {
     extend: {
       colors: {
         buttons: {
-          bgDefault: "#e1e3e6",
-          bgSelected: "#598abb",
+          bgDefault: "rgb(225, 227, 230)", //"#e1e3e6",
+          bgSelected: "rgb(99, 132, 165)", //"#598abb",
           bgHover: "rgba(99, 132, 165, 0.3)",
-          textDefault: "#1A1A1A",
-          textSelected: "rgb(255, 255, 255)",
+          textDefault: "rgb(26, 26, 26)", //"#1A1A1A",
+          textSelected: "rgb(255, 255, 255)", //"#ffffff",
           border: "rgba(45, 45, 45, 0.1)", // Added border style matching serenity-transparent1
           borderSelected: "rgba(192, 42, 42, 0.2)",
         },
         labels: {
-          textDefault: "#1f1f1f",
+          textDefault: "rgb(31, 31, 31)", //"#1f1f1f",
         },
         keys: {
-          bgWhite: "#ffffff",
-          bgBlack: "#2d2d2d",
-          bgSelected: "#598abb",
-          bgHover: "#f0f0f0",
-          bgPressed: "#d0d0d0",
+          bgWhite: "rgb(255, 255, 255)", //"#ffffff",
+          bgBlack: "rgb(45, 45, 45)", //"#2d2d2d",
+          bgSelected: "rgb(99, 132, 165)", //"#598abb",
+          bgHover: "rgb(240, 240, 240)", //"#f0f0f0",
+          bgPressed: "rgb(208, 208, 208)", //"#d0d0d0",
         },
       },
       test: "red",
