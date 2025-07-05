67a29d82d72c8f7111a291f653813c45926da7d1
Aleks Lovestech <alekslovestech@gmail.com>
2025-07-05 10:40:31 +0200

    unify styles

--- src/lib/design/common-styles.ts
+++ src/lib/design/common-styles.ts
@@ -1,18 +1,19 @@
+import { TYPOGRAPHY } from "./Typography";
+
 export const DEBUG_BORDER = "border border-containers-border";
 
 export const COMMON_STYLES = {
-  staff: `flex justify-center items-center px-looser rounded ${DEBUG_BORDER}`,
+  staff: `flex justify-center items-center px-spacious rounded ${DEBUG_BORDER}`,
 
   circularContainer: `p-snug overflow-hidden max-h-full rounded ${DEBUG_BORDER}`,
-  circularInner:
-    "p-2 h-full flex items-center justify-center text-2xl !text-labels-textDefault",
+  circularInner: `p-snug h-full flex items-center justify-center ${TYPOGRAPHY.keyboardText}`,
 
   linearContainer: `w-full h-full flex items-center justify-center overflow-hidden rounded ${DEBUG_BORDER}`,
-  linearInner: "w-full h-full text-2xl",
+  linearInner: `w-full h-full ${TYPOGRAPHY.keyboardText}`,
 
   settingsPanel: `flex overflow-hidden gap-snug p-snug h-full min-h-0 box-border rounded ${DEBUG_BORDER}`,
 
   pageContainer:
     "h-full w-full flex flex-col px-snug md:px-loose lg:px-spacious",
-  pageGrid: `grid p-2 gap-tight overflow-hidden flex-1 ${DEBUG_BORDER}`,
+  pageGrid: `grid p-snug gap-tight overflow-hidden flex-1 ${DEBUG_BORDER}`,
 } as const;
