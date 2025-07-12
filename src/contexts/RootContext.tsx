dc30134041c17da84598558eb63b4a83840dce69
Aleks Dubinskiy <71671601+alekslovestech@users.noreply.github.com>
2025-07-12 16:38:04 +0200

    typography is now handled in Vanila CSS (container queries), better reset (fixes #56) (#64)

--- src/contexts/RootContext.tsx
+++ src/contexts/RootContext.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { ReactNode } from "react";
+import { useGlobalMode } from "@/lib/hooks";
 
 import { DisplayProvider } from "./DisplayContext";
 import { MusicalProvider } from "./MusicalContext";
@@ -9,12 +10,19 @@ import { AudioProvider } from "./AudioContext";
 
 export const RootProvider: React.FC<{ children: ReactNode }> = ({
   children,
-}) => (
-  <MusicalProvider>
-    <AudioProvider>
-      <DisplayProvider>
-        <PresetProvider>{children}</PresetProvider>
-      </DisplayProvider>
-    </AudioProvider>
-  </MusicalProvider>
-);
+}) => {
+  const globalMode = useGlobalMode();
+
+  //keys are used to force re-renders of the children when the global mode changes
+  return (
+    <MusicalProvider key={`musical-${globalMode}`}>
+      <AudioProvider key={`audio-${globalMode}`}>
+        <DisplayProvider key={`display-${globalMode}`}>
+          <PresetProvider key={`preset-${globalMode}`}>
+            {children}
+          </PresetProvider>
+        </DisplayProvider>
+      </AudioProvider>
+    </MusicalProvider>
+  );
+};
