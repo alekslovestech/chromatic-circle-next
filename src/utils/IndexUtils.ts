e102ae8ff1f13e2c1234b39720f21d49fdf205d8
Aleks Lovestech <alekslovestech@gmail.com>
2025-08-19 16:23:36 +0200

    cleanup

--- src/utils/IndexUtils.ts
+++ src/utils/IndexUtils.ts
@@ -4,20 +4,21 @@ import { ChromaticIndex } from "@/types/ChromaticIndex";
 import { ActualIndex, InversionIndex } from "@/types/IndexTypes";
 
 export class IndexUtils {
-  static isBlackKey = (actualIndex: ActualIndex | ChromaticIndex): boolean =>
-    [1, 3, 6, 8, 10].includes(actualIndex % TWELVE);
+  static isBlackKey(actualIndex: ActualIndex | ChromaticIndex): boolean {
+    return [1, 3, 6, 8, 10].includes(actualIndex % TWELVE);
+  }
 
-  static StringWithPaddedIndex = (prefix: string, index: number) =>
-    `${prefix}${String(index).padStart(2, "0")}`;
+  static StringWithPaddedIndex(prefix: string, index: number): string {
+    return `${prefix}${String(index).padStart(2, "0")}`;
+  }
+
   //everything relative to root note
-  static normalizeIndices = (indices: number[]): number[] => {
+  static normalizeIndices(indices: number[]): number[] {
     const rootNote = indices[0];
     return indices.map((note) => (note - rootNote + TWELVE) % TWELVE);
-  };
+  }
 
-  static isNoteInRange = (note: number): boolean => note >= 0 && note < TWENTY4;
-
-  static fitChordToAbsoluteRange = (indices: number[]): number[] => {
+  static fitChordToAbsoluteRange(indices: number[]): number[] {
     let newIndices = this.shiftToRange(indices, 0, TWENTY4);
 
     // Step 3: Check if all notes are now within range
@@ -35,53 +36,63 @@ export class IndexUtils {
 
     // If both fits have the same number of notes, prefer the one that includes the lowest note
     return lowerFit.includes(indices[0]) ? lowerFit : upperFit;
-  };
+  }
 
-  static rootNoteAtInversion = (
+  static rootNoteAtInversion(
     indices: ActualIndex[],
     inversionIndex: InversionIndex
-  ): ActualIndex =>
-    indices[(indices.length - inversionIndex) % indices.length] as ActualIndex;
+  ): ActualIndex {
+    return indices[
+      (indices.length - inversionIndex) % indices.length
+    ] as ActualIndex;
+  }
 
   //put the first note at the end
-  static firstNoteToLast = (indices: number[]): number[] => {
+  static firstNoteToLast(indices: number[]): number[] {
     let newIndices = [...indices] as number[];
     const firstNote = newIndices.shift()!;
     newIndices.push(firstNote + TWELVE);
     return this.shiftToRange(newIndices, -TWELVE, TWELVE);
-  };
+  }
 
-  static areIndicesEqual = (indices1: number[], indices2: number[]): boolean =>
-    indices1.length === indices2.length &&
-    indices1.every((note, index) => note === indices2[index]);
+  static areIndicesEqual(indices1: number[], indices2: number[]): boolean {
+    return (
+      indices1.length === indices2.length &&
+      indices1.every((note, index) => note === indices2[index])
+    );
+  }
+
+  static shiftIndices(indices: number[], shiftAmount: number): number[] {
+    const newIndices = indices.map((index) => index + shiftAmount);
+    return this.fitChordToAbsoluteRange(newIndices);
+  }
 
   //if the new index is already selected, remove it, otherwise add it
-  static ToggleNewIndex = (
+  static ToggleNewIndex(
     selectedNoteIndices: ActualIndex[],
     newIndex: ActualIndex
-  ): ActualIndex[] => {
+  ): ActualIndex[] {
     let updatedIndices = selectedNoteIndices.includes(newIndex)
       ? selectedNoteIndices.filter((index) => index !== newIndex)
       : [...selectedNoteIndices, newIndex];
     updatedIndices = updatedIndices.sort((a, b) => a - b);
     return updatedIndices;
-  };
+  }
 
-  static shiftIndices = (indices: number[], shiftAmount: number): number[] => {
-    const newIndices = indices.map((index) => index + shiftAmount);
-    return this.fitChordToAbsoluteRange(newIndices);
-  };
+  private static isNoteInRange(note: number): boolean {
+    return note >= 0 && note < TWENTY4;
+  }
 
-  private static shiftToRange = (
+  private static shiftToRange(
     indices: number[],
     min: number,
     max: number
-  ): number[] => {
+  ): number[] {
     const shift = indices.some((note) => note >= max)
       ? -TWELVE
       : indices.some((note) => note < min)
       ? TWELVE
       : 0;
     return indices.map((note) => note + shift);
-  };
+  }
 }
