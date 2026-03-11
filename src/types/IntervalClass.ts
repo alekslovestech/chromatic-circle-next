c5ba04c2f13572ddcc84423e135e89bdb3146a23
Aleks Lovestech <alekslovestech@gmail.com>
2026-03-11 09:55:03 +0100

    introduce Intervaldistance

--- src/types/IntervalClass.ts
+++ src/types/IntervalClass.ts
@@ -1,7 +1,17 @@
+import { TWELVE } from "./constants/NoteConstants";
+
+type Branded<K, T> = K & { __brand: T };
 export type IntervalClass = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+export type IntervalDistance = Branded<number, "IntervalDistance">;
 
 export function ixIntervalClass(n: number): IntervalClass {
   if (n < 0 || n > 6 || !Number.isInteger(n))
     throw new Error("Invalid IntervalClass=" + n);
   return n as IntervalClass;
+}
+
+export function ixIntervalDistance(n: number): IntervalDistance {
+  if (n < 0 || n > TWELVE || !Number.isInteger(n))
+    throw new Error("Invalid IntervalDistance=" + n);
+  return n as IntervalDistance;
 }
