bb5ac63151de2c72f72ada33f5abf47133b2ef81
Aleks Lovestech <alekslovestech@gmail.com>
2026-03-14 15:56:49 +0100

    basic ICs tweaked

--- src/utils/visual/IntervalClassColors.ts
+++ src/utils/visual/IntervalClassColors.ts
@@ -7,8 +7,8 @@ export const INTERVAL_CLASS_COLORS: Record<IntervalClass, chroma.Color> = {
   0: chroma.rgb(222, 222, 222), // Unison/Octave - Light Gray
   1: chroma.rgb(219, 20, 61), // m2 / M7 - Crimson
   2: chroma.rgb(255, 166, 0), // M2 / m7 - Orange
-  3: chroma.rgb(255, 255, 0), // m3 / M6 - Bright Yellow
-  4: chroma.rgb(30, 185, 165), // M3 / m6 - Teal
+  3: chroma.rgb(255, 215, 0), // m3 / M6 - Gold
+  4: chroma.rgb(50, 205, 50), // M3 / m6 - Green
   5: chroma.rgb(37, 99, 235), // P4 / P5 - Blue
   6: chroma.rgb(255, 0, 255), // Tritone - Magenta
 };
@@ -17,7 +17,7 @@ export const INTERVAL_CLASS_COLORS: Record<IntervalClass, chroma.Color> = {
 export const INTERVAL_CLASS_DISSONANCE: Record<IntervalClass, number> = {
   0: 0, // Unison/Octave
   1: 0.9, // m2 / M7
-  2: 0.5, // M2 / m7 (softer so sus2/sus4 don’t look harsh)
+  2: 0, // M2 / m7 (softer so sus2/sus4 don’t look harsh)
   3: 0, // m3 / M6
   4: 0, // M3 / m6
   5: 0, // P4 / P5
